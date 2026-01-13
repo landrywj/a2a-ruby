8c4e522e53489f90a7988131a77694d6819e84e7
Wilfrid Landry <wilfridlandry@gmail.com>
2026-01-13 14:42:57 -0800

    Initial commit

---
+++ lib/a2a.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "a2a/version"
+
+module A2a
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/a2a/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module A2a
+  VERSION = "0.1.0"
+end
